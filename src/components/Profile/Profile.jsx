ed2a803efd9a19f5cfc7fe6ffa7ed73ee5a0ce79
Anderson Tome <anderson_tome@outlook.com.br>
2022-09-25 15:16:37 -0300

    Excercicio 7

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,6 +11,7 @@ export const Profile = () => {
       {profiles.map((p) => (
         <div key={p.id}>
           <p>{p.nome}</p>
+          <p>{p.descricao}</p>
           <button onClick={() => handleFollow(p.id) }>{p.seguindo ? "Seguindo" : "Seguir"}</button>
         </div>
       ))}
